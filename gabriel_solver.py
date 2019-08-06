169763d3370dbf2d85cfd9147c8bdcaf7f4bfb4f
Gabi22top <gabriel_s@hotmail.fr>
2019-08-06 15:27:08 +0200

    Sudoku solver with constraint over

--- gabriel_solver.py
+++ gabriel_solver.py
@@ -1,6 +1,7 @@
 """
 Soudry Gabriel
 Sudoku solver using Google OR TOOL Cp solver
+Learning Constraint Optimization
 Project engineering School
 """
 
@@ -8,34 +9,19 @@ from ortools.sat.python import cp_model
 import numpy
 import random
 
-sudoku_3_3 = numpy.array([[0, 1, 3],
-                          [0, 8, 2],
-                          [7, 4, 0]])
-
 
 def display_sudoku(sudoku):
     (i, j) = (0, 0)
-    for i in range(3):
-        for j in range(3):
+    for i in range(9):
+        for j in range(9):
             print(sudoku[i, j], end=" ")
         print("")
 
 
-class SolutionPrinter(cp_model.CpSolverSolutionCallback):
-
-    def __init__(self, variables):
-        cp_model.CpSolverSolutionCallback.__init__(self)
-        self.__variables = variables
-        self.__solution_count = 0
-
-    def SolutionCount(self):
-        return self.__solution_count
-
-
 # Allow to put initial constraint of the Sudoku
 def initialize_sudoku(model_or_tool, sudoku_init, sudoku_to_do):
-    for i in range(3):
-        for j in range(3):
+    for i in range(9):
+        for j in range(9):
             if sudoku_to_do[i][j] != 0:  # If case of sudoku is filled
                 sudoku_init[i][j] = model_or_tool.NewIntVar(int(sudoku_to_do[i][j]), int(sudoku_to_do[i][j]),
                                                             'column: %i' % i)
@@ -44,31 +30,56 @@ def initialize_sudoku(model_or_tool, sudoku_init, sudoku_to_do):
 
 def solveSudoku(sudoku):
     model = cp_model.CpModel()
-    sudoku2 = [[model.NewIntVar(1, 9, 'column: %i' % i) for i in range(3)] for j in range(3)]
+    sudoku2 = [[model.NewIntVar(1, 9, 'column: %i' % i) for i in range(9)] for j in range(9)]
     sudoku = initialize_sudoku(model, sudoku2, sudoku)
-    for i in range(3):
+
+    # Constraint in line
+    for i in range(9):
         line = []
-        for j in range(3):
+        for j in range(9):
             line.append(sudoku[i][j])
         model.AddAllDifferent(line)
 
-    for index in range(1):
+    # Constraint in column
+    for i in range(9):
+        column = []
+        for j in range(9):
+            column.append(sudoku[j][i])
+
+    # Constraint in sector
+    for index in range(9):
         sector = []
         for i in [(index // 3) * 3, (index // 3) * 3 + 1, (index // 3) * 3 + 2]:
             for j in [(index % 3) * 3, (index % 3) * 3 + 1, (index % 3) * 3 + 2]:
                 sector.append(sudoku[i][j])
                 model.AddAllDifferent(sector)
 
-        solver = cp_model.CpSolver()
-        status = solver.Solve(model)
+    # Initialize the solver
+    solver = cp_model.CpSolver()
+
+    # Solving
+    status = solver.Solve(model)
 
     print("==================")
     if status == cp_model.FEASIBLE:
-        for i in range(3):
-            for j in range(3):
+        for i in range(9):
+            for j in range(9):
                 print(solver.Value(sudoku[i][j]), end=" ")
             print()
 
-display_sudoku(sudoku_3_3)
+
+# Example of solving :
+
+sudoku_to_solve = numpy.array([[0, 0, 4, 1, 0, 0, 7, 9, 3],
+                               [0, 9, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 7, 2, 0, 0, 8, 0, 0],
+                               [6, 0, 9, 0, 4, 0, 0, 0, 0],
+                               [0, 3, 0, 5, 0, 9, 0, 6, 0],
+                               [0, 0, 0, 0, 3, 0, 2, 0, 9],
+                               [0, 0, 6, 0, 0, 8, 1, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 3, 0],
+                               [5, 8, 2, 0, 0, 6, 9, 0, 0]])
+
+display_sudoku(sudoku_to_solve)
 print("==================")
-solveSudoku(sudoku_3_3)
+solveSudoku(sudoku_to_solve)
